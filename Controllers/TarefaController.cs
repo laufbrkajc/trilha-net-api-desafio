60736dd50e3721fb2fea709a69a2d6ba59eca453
Gabriel <podryto@protonmail.com>
2022-12-21 23:13:44 -0300

    Implementado o endpoint Post Criar e corrigida a string de conexão.

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -18,9 +18,6 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
-            // caso contrário retornar OK com a tarefa encontrada
             var tarefaEncontrada = _context.Tarefas.Find(id);
 
             if (tarefaEncontrada != null) {
@@ -68,7 +65,9 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-            // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+            _context.Add(tarefa);
+            _context.SaveChanges();
+
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
